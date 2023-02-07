031dd1ad9478f06ac701656bdeb25d9e5d6ff88c
Junio Sousa <juniosousa120@gmail.com>
2023-02-07 05:36:38 -0300

    Finalizar navegação

--- js/slide.js
+++ js/slide.js
@@ -6,6 +6,10 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, moviment: 0 };
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
+  }
+
   moveSlide(distX) {
     this.dist.movimentPosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -28,6 +32,7 @@ export default class Slide {
     }
 
     this.wrapper.addEventListener(movetype, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
@@ -43,6 +48,18 @@ export default class Slide {
     const eventEnd = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrapper.removeEventListener(eventEnd, this.onMove);
     this.dist.finalPosition = this.dist.movimentPosition;
+    this.transition(true);
+    this.changeSlideEnd();
+  }
+
+  changeSlideEnd() {
+    if (this.dist.moviment > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.moviment < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.actual);
+    }
   }
 
   addSlideEvent() {
@@ -84,6 +101,14 @@ export default class Slide {
     this.moveSlide(activeSlide.position);
     this.slideIndex(index);
     this.dist.finalPosition = activeSlide.position;
+  }
+
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
   }
 
   init() {
